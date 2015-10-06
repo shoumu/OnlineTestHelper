714d4aa8d58a30943984be05c44e6f3a1548793f
shoumu <shoumuzyq@gmail.com>
2015-10-07 00:40:22 +0800

    change the status of scroller bar

--- js/inject-code.js
+++ js/inject-code.js
@@ -2,7 +2,7 @@ var workPageUrl = chrome.extension.getURL('html/work.html');
 
 var iframe = document.createElement('iframe');
 iframe.src = workPageUrl;
-iframe.scrolling = "yes";
+iframe.scrolling = "auto";
 iframe.width = 500;
 iframe.height = 500;
 document.body.appendChild(iframe);
